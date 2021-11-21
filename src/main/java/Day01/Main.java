e8908b36a984dc198a5c8ca651275fbc6b3ad7d2
Attila Gönczi-Kovács <rogkacs@gmail.com>
2021-11-21 12:58:35 +0100

    Day01
    Készíts egy day01.PositiveNumberContainer osztályt. Ebben az osztályban, legyen egy lista, ami valós számokat tárol, illetve legyen egy add metódus, ami egy számot hozzáad a listához. Legyen egy day01.Main nevű osztályod, benne egy main metódussal. A feladat, az hogy addig kérj be számokat és tárold el a listában, amíg pozitív számokat gépel be a felhasználó. Amint érkezik egy nem pozitív szám írd ki az eddig begépelt számokat.

---
+++ src/main/java/Day01/Main.java
@@ -0,0 +1,26 @@
+package Day01;
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        PositiveNumberContainer container = new PositiveNumberContainer();
+
+        System.out.println("Adj meg pozitív egész számokat, enterrel elválasztva.");
+
+        double number;
+        boolean positive = true;
+
+        while (positive) {
+            number = sc.nextDouble();
+            if (number > 0) {
+                container.add(number);
+            } else {
+                positive = false;
+            }
+        }
+        System.out.println(container);
+    }
+}
---
+++ src/main/java/Day01/PositiveNumberContainer.java
@@ -0,0 +1,21 @@
+package Day01;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PositiveNumberContainer {
+    private final List<Double> numbers = new ArrayList<>();
+
+    public void add(Double number) {
+        numbers.add(number);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (Double num : numbers) {
+            sb.append(num + " ");
+        }
+        return new String(sb);
+    }
+}
